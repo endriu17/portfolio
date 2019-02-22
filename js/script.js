8cd3d7ee3232cef1c1dc3ce0d4bbe0cc9f3adf3b
endriu <aolaszewski@gmail.com>
2019-02-23 00:38:42 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(".sidenav .nav-link").on("click", function() {
+  $(".sidenav")
+    .find(".active")
+    .removeClass("active");
+  $(this).addClass("active");
+});
+
+".nav-tabs .nav-item".on("click", function() {
+  $(".nav")
+    .find(".active")
+    .removeClass("active");
+  $(this).addClass("active");
+});
