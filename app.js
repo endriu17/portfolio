c6ff836736201dcebfd03184ad0c0cf688370d68
endriu <aolaszewski@gmail.com>
2019-06-24 18:55:54 +0200

    add backend

---
+++ app.js
@@ -0,0 +1,105 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const os = require('os');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/email', (req, res) => {
+  let lang = process.env.LANGUAGE;
+
+  const now = new Date()
+    .toISOString()
+    .replace(/T/, ' ')
+    .replace(/\..+/, '');
+  const output = `
+  <p>Your site was visited</p>
+  <p>Date: ${now} language: ${lang} hostname: ${os.hostname()} System: ${os.platform()} </p>
+  <p>Browser ${req.headers['user-agent']}</p>
+`;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.MY_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MY_USER,
+      pass: process.env.MY_PASSWORD
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: `"Portfolio Contact" <${process.env.MY_USER}>`, // sender address
+    cc: process.env.MY_USER, // list of receivers
+    subject: 'Visit site request', 
+    text: 'Your message was send', 
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(info);
+  });
+});
+
+app.post('/contact', (req, res) => {
+  console.log(req.body.email);
+  const now = new Date()
+    .toISOString()
+    .replace(/T/, ' ')
+    .replace(/\..+/, '');
+  const output = `
+    <p>Your contact request to Andrzej Olaszewski</p>
+    <h3>Message Details</h3>
+    <div>  
+      <h4>Name: ${req.body.name}</h4>
+      <h4>Email: ${req.body.email}</h4>
+    </div>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    <br>
+    <p>Date ${now}</p>
+    <p>Thank you and I will try to get in touch with you as soon as possible</p>
+  `;
+
+  let transporter = nodemailer.createTransport({
+    host: process.env.MY_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MY_USER,
+      pass: process.env.MY_PASSWORD
+    }
+  });
+
+  let mailOptions = {
+    from: `"Portfolio Contact" <${process.env.MY_USER}>`, 
+    to: req.body.email,
+    cc: process.env.MY_USER,
+    subject: 'Contact Request',
+    text: 'Your message was send',
+    html: output 
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(info);
+    res.redirect('/#contact');
+  });
+});
+
+app.listen(3005, () => console.log('Server started on port 3005...'));
--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-$(".sidenav .nav-link").on("click", function() {
-  $(".sidenav")
-    .find(".active")
-    .removeClass("active");
-  $(this).addClass("active");
+$('.sidenav .nav-link').on('click', function() {
+  $('.sidenav')
+    .find('.active')
+    .removeClass('active');
+  $(this).addClass('active');
 });
 
-$(".nav-tabs .nav-item").on("click", function() {
-  $(".nav")
-    .find(".active")
-    .removeClass("active");
-  $(this).addClass("active");
+$('.nav-tabs .nav-item').on('click', function() {
+  $('.nav')
+    .find('.active')
+    .removeClass('active');
+  $(this).addClass('active');
 });
 
+window.addEventListener('load', function(event) {
+  const p = window.fetch('http://localhost:3005/email');
+  p.then(response => {
+    console.info('got a response', response);
+  });
+  console.log(
+    'Web page fully Loaded. HTML, Javascript, CSS, Images, Iframes and objects are fully loaded.'
+  );
+});
